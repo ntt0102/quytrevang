f456f8f892992f2eb430bee2c112066687e388f9
Tho Nguyen <ntt0102@gmail.com>
2022-11-09 20:39:54 +0700

    fix number format ,0

--- resources/js/filters/numberVnFormat.js
+++ resources/js/filters/numberVnFormat.js
@@ -1,5 +1,6 @@
 export default function numberVnFormat(number, fixed = -1) {
     if (number == undefined) number = 0;
     if (number != 0 && fixed > -1) number = number.toFixed(fixed);
-    return number.toString().replaceAll(".", ",");
+    number = number.toString().replaceAll(".", ",");
+    return fixed == 1 ? number.replace(",0", "") : number;
 }
