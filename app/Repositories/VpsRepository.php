333a02c1bdd912243ef15d97e35d430ee69b6950
Tho Nguyen <ntt0102@gmail.com>
2022-11-13 15:48:07 +0700

    add vol10

--- app/Repositories/VpsRepository.php
+++ app/Repositories/VpsRepository.php
@@ -29,7 +29,8 @@ class VpsRepository extends CoreRepository
     {
         return [
             'price' => $this->model->where('type', 0)->get(),
-            'volume' => $this->model->where('type', 1)->get()
+            'volume' => $this->model->where('type', 1)->get(),
+            'vol10' => $this->model->where('type', 2)->get()
         ];
     }
 
