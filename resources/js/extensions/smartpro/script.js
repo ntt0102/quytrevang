a85c264d02d05ee6e2e7a2d93080a9e905654617
Tho Nguyen <ntt0102@gmail.com>
2022-11-18 12:52:33 +0700

    revert

--- resources/js/extensions/smartpro/script.js
+++ resources/js/extensions/smartpro/script.js
@@ -447,20 +447,17 @@ function getData() {
             .then(response => response.json())
             .then(json => {
                 console.log("getData", json);
-                // json.price.map(item => {
-                //     item.x = moment(item.x);
-                //     return item;
-                // });
-                // json.volume.map(item => {
-                //     item.x = moment(item.x);
-                //     return item;
-                // });
-                mChart.data.datasets[0].data = json.ATO.price;
-                mChart.data.datasets[1].data = json.ATO.volume;
-                mChart.data.datasets[2].data = json.ATO.vol10;
-                mChart.data.datasets[3].data = json.ATC.price;
-                mChart.data.datasets[4].data = json.ATC.volume;
-                mChart.data.datasets[5].data = json.ATC.vol10;
+                json.price.map(item => {
+                    item.x = moment(item.x);
+                    return item;
+                });
+                json.volume.map(item => {
+                    item.x = moment(item.x);
+                    return item;
+                });
+                mChart.data.datasets[0].data = json.price;
+                mChart.data.datasets[1].data = json.volume;
+                mChart.data.datasets[2].data = json.vol10;
                 mChart.update("none");
                 toggleSpinner(false);
                 resolve();
