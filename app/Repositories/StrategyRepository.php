a9f5dc43de02c61bae7d5acbf4dfdb785b530e1e
Tho Nguyen <ntt0102@gmail.com>
2023-01-08 22:40:30 +0700

    fix

--- app/Repositories/StrategyRepository.php
+++ app/Repositories/StrategyRepository.php
@@ -30,10 +30,11 @@ class StrategyRepository extends CoreRepository
      */
     public function getStrategies($trend, $momentum, $atc)
     {
-        return $this->model->where('trend', $trend)
-            ->where('momentum', $momentum > 0 ? '>=' : '<=', 0)
-            ->where('atc', $atc > 0 ? '>=' : '<=', 0)
-            ->orderBy('ato', 'DESC')
+        $model = $this->model->where('trend', $trend);
+        if ($momentum != null) $model = $model->where('momentum', $momentum > 0 ? '>=' : '<=', 0);
+        if ($atc != null) $model = $model->where('atc', $atc > 0 ? '>=' : '<=', 0);
+        return
+            $model->orderBy('ato', 'DESC')
             ->get();
     }
 
