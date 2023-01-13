f42778dacc50ecbb4cdd5fa092b98ba2aa2ed63f
Tho Nguyen <ntt0102@gmail.com>
2023-01-13 11:16:44 +0700

    fix vps

--- app/Models/Vps.php
+++ app/Models/Vps.php
@@ -23,10 +23,7 @@ class Vps extends CoreModel
 
     public function getXAttribute()
     {
-        // return Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('Y-m-d H:i:s');
-        // return  $date->format('Y-m-d');
         return $this->time->format('Y-m-d H:i:s');
-        //
     }
     public function getYAttribute()
     {
