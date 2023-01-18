79e717a0d6cdf117e152096c6fba04c288d2fde5
Tho Nguyen <ntt0102@gmail.com>
2023-01-18 17:35:57 +0700

    fix

--- app/Services/Special/SocketService.php
+++ app/Services/Special/SocketService.php
@@ -158,6 +158,6 @@ class SocketService extends CoreService
     private function inSocketTimeLimit()
     {
         $startSocketTime = strtotime(get_global_value('startSocketTime'));
-        return time() <= $startSocketTime  + 5 * 60 + 30;
+        return time() <= $startSocketTime  + 10 * 60 + 30;
     }
 }
