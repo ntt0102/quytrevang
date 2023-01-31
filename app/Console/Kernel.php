5a4809ce579e4db7b4f4a536ef2ea81e7944ce93
Tho Nguyen <ntt0102@gmail.com>
2023-01-31 14:38:49 +0700

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,16 +34,17 @@ class Kernel extends ConsoleKernel
             if ($openingMarketFlag) {
                 set_global_value('bidPrice', '0');
                 set_global_value('askPrice', '0');
+                set_global_value('reportedTradingFlag', '0');
                 app(\App\Repositories\VpsRepository::class)->clear();
             }
-        })->dailyAt('08:40');
+        })->dailyAt('08:55');
         //
-        $schedule->call(function () {
-            if (get_global_value('openingMarketFlag') == '1') {
-                set_global_value('reportedTradingFlag', '0');
-                app(\App\Services\VpsService::class)->setAtoStrategy();
-            }
-        })->dailyAt('11:35');
+        // $schedule->call(function () {
+        //     if (get_global_value('openingMarketFlag') == '1') {
+        //         set_global_value('reportedTradingFlag', '0');
+        //         app(\App\Services\VpsService::class)->setAtoStrategy();
+        //     }
+        // })->dailyAt('11:35');
         //
         $schedule->call(function () {
             if (get_global_value('openingMarketFlag') == '1') {
@@ -56,11 +57,11 @@ class Kernel extends ConsoleKernel
         //
         $schedule->call(function () {
             if (get_global_value('openingMarketFlag') == '1') {
-                app(\App\Services\Admin\ShareService::class)->getData();
-                app(\App\Services\Admin\Vn30f1mService::class)->getData();
+                // app(\App\Services\Admin\ShareService::class)->getData();
+                // app(\App\Services\Admin\Vn30f1mService::class)->getData();
                 app(\App\Services\VpsService::class)->exportToCsv();
             }
-        })->dailyAt('14:47');
+        })->dailyAt('14:35');
     }
 
     /**
