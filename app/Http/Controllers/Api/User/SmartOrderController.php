4000081c62ffa929385946f451ec8424916cb334
Tho Nguyen <ntt0102@gmail.com>
2023-03-17 18:29:38 +0700

    fix

--- app/Http/Controllers/Api/User/SmartOrderController.php
+++ app/Http/Controllers/Api/User/SmartOrderController.php
@@ -55,28 +55,15 @@ class SmartOrderController extends CoreController
     }
 
     /**
-     * get Volume By Price
-     *
-     * @param \Illuminate\Http\Request $request
-     * 
-     * @return \Illuminate\Http\Response
-     */
-    public function getVolumeByPrice(Request $request)
-    {
-        $data = $this->smartOrderService->getVolumeByPrice();
-        return $this->sendResponse($data);
-    }
-
-    /**
      * Get, set and Clear Data
      *
      * @param \Illuminate\Http\Request $request
      * 
      * @return \Illuminate\Http\Response
      */
-    public function controlData(Request $request)
+    public function getChartData(Request $request)
     {
-        $data = $this->smartOrderService->controlData($request);
+        $data = $this->smartOrderService->getChartData($request);
         return $this->sendResponse($data);
     }
 }
