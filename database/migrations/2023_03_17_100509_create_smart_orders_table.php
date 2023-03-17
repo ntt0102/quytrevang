788b02fec8a54c667fadb1637e66c3a64188a6ed
Tho Nguyen <ntt0102@gmail.com>
2023-03-17 10:20:01 +0700

    add smart orders table

---
+++ database/migrations/2023_03_17_100509_create_smart_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSmartOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('smart_orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_code');
+            $table->date('started_at');
+            $table->string('periods');
+            $table->integer('device_limit');
+            $table->json('devices');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('smart_orders');
+    }
+}
