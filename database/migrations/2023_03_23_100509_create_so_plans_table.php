84338ae6588b42f0012f5dfe6d138a2873acd1b2
Tho Nguyen <ntt0102@gmail.com>
2023-03-23 11:02:14 +0700

    add so_plans

---
+++ database/migrations/2023_03_23_100509_create_so_plans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSoPlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('so_plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('months');
+            $table->integer('price');
+            $table->json('renewal_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('so_plans');
+    }
+}
