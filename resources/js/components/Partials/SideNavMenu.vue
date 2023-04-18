59b3ca7b37fe58e8db31f291c7c6992b0d4964f1
Tho Nguyen <ntt0102@gmail.com>
2023-04-18 12:26:01 +0700

    fix navigation

--- resources/js/components/Partials/SideNavMenu.vue
+++ resources/js/components/Partials/SideNavMenu.vue
@@ -1,81 +1,87 @@
 <template>
-  <div class="dx-swatch-additional side-navigation-menu" @click="forwardClick">
-    <slot />
-    <div class="menu-container" @mouseover="forwardClick">
-      <DxTreeView
-        ref="treeView"
-        :items="items"
-        key-expr="name"
-        selection-mode="single"
-        :focus-state-enabled="false"
-        expand-event="click"
-        @item-click="handleItemClick"
-        width="100%"
-      >
-        <template #item="item">
-          <div class="item">
-            <i class="dx-icon" :class="item.data.icon" />
-            <span>{{ item.data.text }}</span>
-            <span
-              v-if="
-                item.data.name === 'users' && sidenav.signingUsersNumber > 0
-              "
-              class="badge"
-              :title="
-                $t('admin.users.signingUsers').replace(
-                  '{count}',
-                  sidenav.signingUsersNumber
-                )
-              "
+    <div
+        class="dx-swatch-additional side-navigation-menu"
+        @click="forwardClick"
+    >
+        <slot />
+        <div class="menu-container" @mouseover="forwardClick">
+            <DxTreeView
+                ref="treeView"
+                :items="items"
+                key-expr="name"
+                selection-mode="single"
+                :focus-state-enabled="false"
+                expand-event="click"
+                @item-click="handleItemClick"
+                width="100%"
             >
-              <div class="container">
-                <div class="item">
-                  {{ sidenav.signingUsersNumber }}
-                </div>
-                <div class="circle"></div>
-              </div>
-            </span>
-            <span
-              v-if="
-                item.data.name === 'contracts' &&
-                sidenav.confirmingContractsNumber > 0
-              "
-              class="badge"
-              :title="
-                $t('admin.contracts.confirmingContracts').replace(
-                  '{count}',
-                  sidenav.confirmingContractsNumber
-                )
-              "
-              ><div class="container">
-                <div class="item">
-                  {{ sidenav.confirmingContractsNumber }}
-                </div>
-                <div class="circle"></div></div
-            ></span>
-            <span
-              v-if="
-                item.data.name === 'comments' &&
-                sidenav.unreadCommentsNumber > 0
-              "
-              class="badge"
-              :title="
-                $t('admin.comments.unreadComments').replace(
-                  '{count}',
-                  sidenav.unreadCommentsNumber
-                )
-              "
-              ><div class="container">
-                <div class="item">
-                  {{ sidenav.unreadCommentsNumber }}
-                </div>
-                <div class="circle"></div></div
-            ></span>
-          </div>
-        </template>
-      </DxTreeView>
+                <template #item="item">
+                    <div class="item">
+                        <i class="dx-icon" :class="item.data.icon" />
+                        <span>{{ item.data.text }}</span>
+                        <span
+                            v-if="
+                                item.data.name === 'users' &&
+                                    sidenav.signingUsersNumber > 0
+                            "
+                            class="badge"
+                            :title="
+                                $t('admin.users.signingUsers').replace(
+                                    '{count}',
+                                    sidenav.signingUsersNumber
+                                )
+                            "
+                        >
+                            <div class="container">
+                                <div class="item">
+                                    {{ sidenav.signingUsersNumber }}
+                                </div>
+                                <div class="circle"></div>
+                            </div>
+                        </span>
+                        <span
+                            v-if="
+                                item.data.name === 'contracts' &&
+                                    sidenav.confirmingContractsNumber > 0
+                            "
+                            class="badge"
+                            :title="
+                                $t(
+                                    'admin.contracts.confirmingContracts'
+                                ).replace(
+                                    '{count}',
+                                    sidenav.confirmingContractsNumber
+                                )
+                            "
+                            ><div class="container">
+                                <div class="item">
+                                    {{ sidenav.confirmingContractsNumber }}
+                                </div>
+                                <div class="circle"></div></div
+                        ></span>
+                        <span
+                            v-if="
+                                item.data.name === 'comments' &&
+                                    sidenav.unreadCommentsNumber > 0
+                            "
+                            class="badge"
+                            :title="
+                                $t('admin.comments.unreadComments').replace(
+                                    '{count}',
+                                    sidenav.unreadCommentsNumber
+                                )
+                            "
+                            ><div class="container">
+                                <div class="item">
+                                    {{ sidenav.unreadCommentsNumber }}
+                                </div>
+                                <div class="circle"></div></div
+                        ></span>
+                    </div>
+                </template>
+            </DxTreeView>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -84,79 +90,87 @@ import DxTreeView from "devextreme-vue/ui/tree-view";
 import navigation from "../../configs/navigation.js";
 
 export default {
-  components: {
-    DxTreeView,
-  },
-  props: {
-    compactMode: Boolean,
-  },
-  data() {
-    return {
-      items: null,
-    };
-  },
-  created() {
-    this.items = navigation
-      .filter((item) => {
-        return (
-          (!item.permission ||
-            (!!item.permission &&
-              this.permissions.includes(item.permission))) &&
-          (!item.level || (!!item.level && this.level >= item.level))
-        );
-      })
-      .map((item) => {
-        return { ...item, expanded: false };
-      });
-  },
-  mounted() {
-    this.treeView = this.$refs.treeView && this.$refs.treeView.instance;
-    this.updateSelection();
-    if (this.compactMode) {
-      this.treeView.collapseAll();
-    }
-  },
-  watch: {
-    $route() {
-      this.updateSelection();
+    components: {
+        DxTreeView
     },
-    compactMode() {
-      if (this.compactMode) {
-        this.treeView.collapseAll();
-      } else {
+    props: {
+        compactMode: Boolean
+    },
+    data() {
+        return {
+            items: null
+        };
+    },
+    created() {
+        this.items = this.filter(navigation).map(item => ({
+            ...item,
+            expanded: false
+        }));
+    },
+    mounted() {
+        this.treeView = this.$refs.treeView && this.$refs.treeView.instance;
         this.updateSelection();
-      }
+        if (this.compactMode) {
+            this.treeView.collapseAll();
+        }
     },
-  },
-  computed: {
-    ...mapGetters("User.layout", ["sidenav"]),
-    ...mapGetters("Auth", ["permissions", "level"]),
-  },
-  methods: {
-    forwardClick(...args) {
-      this.$emit("click", args);
+    watch: {
+        $route() {
+            this.updateSelection();
+        },
+        compactMode() {
+            if (this.compactMode) {
+                this.treeView.collapseAll();
+            } else {
+                this.updateSelection();
+            }
+        }
     },
-
-    handleItemClick(e) {
-      if (!e.itemData.name || this.compactMode) {
-        return;
-      }
-      if (e.itemData.name == "refresh") window.location.reload();
-      else this.$router.push({ name: e.itemData.name });
-
-      const pointerEvent = e.event;
-      pointerEvent.stopPropagation();
+    computed: {
+        ...mapGetters("User.layout", ["sidenav"]),
+        ...mapGetters("Auth", ["permissions", "level"])
     },
+    methods: {
+        filter(nav) {
+            return nav.reduce((c, item) => {
+                const condition =
+                    (!item.permissions ||
+                        (!!item.permissions &&
+                            this.permissions.some(p =>
+                                item.permissions.includes(p)
+                            ))) &&
+                    (!item.level || (!!item.level && this.level >= item.level));
+                if (condition) {
+                    if (!!item.items) item.items = this.filter(item.items);
+                    c.push(item);
+                }
+                return c;
+            }, []);
+        },
+        forwardClick(...args) {
+            this.$emit("click", args);
+        },
 
-    updateSelection() {
-      if (!this.treeView) {
-        return;
-      }
+        handleItemClick(e) {
+            if (!e.itemData.name || this.compactMode) {
+                return;
+            }
+            if (e.itemData.name == "refresh") window.location.reload();
+            else this.$router.push({ name: e.itemData.name });
 
-      this.treeView.selectItem(this.$route.name);
-      this.treeView.expandItem(this.$route.name);
-    },
-  },
+            const pointerEvent = e.event;
+            pointerEvent.stopPropagation();
+        },
+
+        updateSelection() {
+            if (!this.treeView) {
+                return;
+            }
+
+            this.treeView.selectItem(this.$route.name);
+            this.treeView.expandItem(this.$route.name);
+        }
+    }
 };
 </script>
 
@@ -164,148 +178,151 @@ export default {
 @import "../../../sass/variables.scss";
 
 .side-navigation-menu {
-  display: flex;
-  flex-direction: column;
-  min-height: 100%;
-  height: 100%;
-  width: 250px !important;
-
-  .menu-container {
-    background-color: $base-bg;
-    background-image: url("../../../images/background-sidebar.jpg");
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: left top;
-    min-height: 100%;
     display: flex;
-    flex: 1;
-    padding-bottom: 76px;
+    flex-direction: column;
+    min-height: 100%;
+    height: 100%;
+    width: 250px !important;
 
-    .dx-treeview {
-      // ## Long text positioning
-      white-space: nowrap;
-      // ##
-
-      // ## Icon width customization
-      .dx-treeview-item {
-        padding-left: 0;
-        padding-right: 0;
-
-        .dx-icon {
-          width: $side-panel-min-width !important;
-          margin: 0 !important;
-        }
-      }
-      // ##
-
-      // ## Arrow customization
-      .dx-treeview-node {
-        padding: 0 0 !important;
-      }
-
-      .dx-treeview-toggle-item-visibility {
-        right: 10px;
-        left: auto;
-      }
-
-      .dx-rtl .dx-treeview-toggle-item-visibility {
-        left: 10px;
-        right: auto;
-      }
-      // ##
-
-      // ## Item levels customization
-      .dx-treeview-node {
-        &[aria-level="1"] {
-          font-weight: bold;
-          border-bottom: 1px solid $base-border-color;
-        }
-
-        &[aria-level="2"] .dx-treeview-item-content {
-          font-weight: normal;
-          padding: 0 calc($side-panel-min-width/4);
-        }
-      }
-      // ##
-    }
-
-    // ## Selected & Focuced items customization
-    .dx-treeview {
-      .dx-treeview-node-container {
-        .dx-treeview-node {
-          &.dx-state-selected:not(.dx-state-focused) > .dx-treeview-item {
-            background: transparent;
-          }
-
-          &.dx-state-selected > .dx-treeview-item * {
-            color: $base-accent;
-          }
-
-          &:not(.dx-state-focused) > .dx-treeview-item.dx-state-hover {
-            background-color: lighten($base-bg, 4);
-          }
-        }
-      }
-    }
-
-    .dx-theme-generic .dx-treeview {
-      .dx-treeview-node-container
-        .dx-treeview-node.dx-state-selected.dx-state-focused
-        > .dx-treeview-item
-        * {
-        color: inherit;
-      }
-    }
-    // ##
-  }
-
-  .item {
-    position: relative;
-    font-size: 18px;
-
-    .badge {
-      position: absolute;
-      top: -45px;
-      left: -5px;
-
-      .container {
-        width: 100px;
-        height: 100px;
+    .menu-container {
+        background-color: $base-bg;
+        background-image: url("../../../images/background-sidebar.jpg");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: left top;
+        min-height: 100%;
         display: flex;
-        align-items: center;
-        justify-content: center;
-        overflow: hidden;
+        flex: 1;
+        padding-bottom: 76px;
+
+        .dx-treeview {
+            // ## Long text positioning
+            white-space: nowrap;
+            // ##
+
+            // ## Icon width customization
+            .dx-treeview-item {
+                padding-left: 0;
+                padding-right: 0;
+
+                .dx-icon {
+                    width: $side-panel-min-width !important;
+                    margin: 0 !important;
+                }
+            }
+            // ##
+
+            // ## Arrow customization
+            .dx-treeview-node {
+                padding: 0 0 !important;
+            }
+
+            .dx-treeview-toggle-item-visibility {
+                right: 10px;
+                left: auto;
+            }
+
+            .dx-rtl .dx-treeview-toggle-item-visibility {
+                left: 10px;
+                right: auto;
+            }
+            // ##
+
+            // ## Item levels customization
+            .dx-treeview-node {
+                &[aria-level="1"] {
+                    font-weight: bold;
+                    border-bottom: 1px solid $base-border-color;
+                }
+
+                &[aria-level="2"] .dx-treeview-item-content {
+                    font-weight: normal;
+                    padding: 0 calc($side-panel-min-width/4);
+                }
+            }
+            // ##
+        }
+
+        // ## Selected & Focuced items customization
+        .dx-treeview {
+            .dx-treeview-node-container {
+                .dx-treeview-node {
+                    &.dx-state-selected:not(.dx-state-focused)
+                        > .dx-treeview-item {
+                        background: transparent;
+                    }
+
+                    &.dx-state-selected > .dx-treeview-item * {
+                        color: $base-accent;
+                    }
+
+                    &:not(.dx-state-focused)
+                        > .dx-treeview-item.dx-state-hover {
+                        background-color: lighten($base-bg, 4);
+                    }
+                }
+            }
+        }
+
+        .dx-theme-generic .dx-treeview {
+            .dx-treeview-node-container
+                .dx-treeview-node.dx-state-selected.dx-state-focused
+                > .dx-treeview-item
+                * {
+                color: inherit;
+            }
+        }
+        // ##
+    }
+
+    .item {
         position: relative;
-      }
+        font-size: 18px;
 
-      .item {
-        z-index: 1000;
-        padding: 0px 5px;
-        background-color: red;
-        border-radius: 50%;
-        color: white !important;
-      }
+        .badge {
+            position: absolute;
+            top: -45px;
+            left: -5px;
 
-      .circle {
-        border-radius: 50%;
-        background-color: red;
-        width: 30px;
-        height: 30px;
-        position: absolute;
-        opacity: 0;
-        animation: scaleIn 1s infinite cubic-bezier(0.36, 0.11, 0.89, 0.32);
-      }
+            .container {
+                width: 100px;
+                height: 100px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                overflow: hidden;
+                position: relative;
+            }
+
+            .item {
+                z-index: 1000;
+                padding: 0px 5px;
+                background-color: red;
+                border-radius: 50%;
+                color: white !important;
+            }
+
+            .circle {
+                border-radius: 50%;
+                background-color: red;
+                width: 30px;
+                height: 30px;
+                position: absolute;
+                opacity: 0;
+                animation: scaleIn 1s infinite
+                    cubic-bezier(0.36, 0.11, 0.89, 0.32);
+            }
+        }
     }
-  }
-  @keyframes scaleIn {
-    from {
-      transform: scale(0.5, 0.5);
-      opacity: 0.5;
+    @keyframes scaleIn {
+        from {
+            transform: scale(0.5, 0.5);
+            opacity: 0.5;
+        }
+        to {
+            transform: scale(2, 2);
+            opacity: 0;
+        }
     }
-    to {
-      transform: scale(2, 2);
-      opacity: 0;
-    }
-  }
 }
 </style>
--- resources/js/configs/navigation.js
+++ resources/js/configs/navigation.js
@@ -10,58 +10,73 @@ export default [
         text: i18n.t("user.profile.title"),
         name: "profile",
         icon: "far fa-id-card",
-        permission: "profile@control",
+        permissions: ["profile@control"],
         level: 3
     },
     {
         text: i18n.t("user.contract.title"),
         name: "contract",
         icon: "far fa-usd-circle",
-        permission: "contract@control",
+        permissions: ["contract@control"],
         level: 5
     },
     {
-        text: i18n.t("admin.users.title"),
-        name: "users",
-        icon: "far fa-user-cog",
-        permission: "users@control"
-    },
-    {
-        text: i18n.t("admin.contracts.title"),
-        name: "contracts",
-        icon: "far fa-file-invoice-dollar",
-        permission: "contracts@control"
-    },
-    {
-        text: i18n.t("admin.finbooks.title"),
-        name: "finbooks",
-        icon: "far fa-book",
-        permission: "finbooks@control"
-    },
-    {
-        text: i18n.t("admin.trades.title"),
-        name: "trades",
-        icon: "far fa-analytics",
-        permission: "trades@view"
-    },
-    {
-        text: i18n.t("admin.smartorders.title"),
-        name: "smartorders",
-        icon: "far fa-store",
-        permission: "smartorders@control"
-    },
-    {
-        text: i18n.t("admin.comments.title"),
-        name: "comments",
-        icon: "far fa-comment",
-        permission: "comments@control"
-    },
-    {
-        text: i18n.t("admin.settings.title"),
-        name: "settings",
-        icon: "far fa-cog",
-        permission: "setting@control",
-        level: 3
+        text: "Quản lý",
+        icon: "far fa-tools",
+        permissions: [
+            "users@control",
+            "contracts@control",
+            "finbooks@control",
+            "trades@view",
+            "smartorders@control",
+            "comments@control",
+            "setting@control"
+        ],
+        items: [
+            {
+                text: i18n.t("admin.users.title"),
+                name: "users",
+                icon: "far fa-user-cog",
+                permissions: ["users@control"]
+            },
+            {
+                text: i18n.t("admin.contracts.title"),
+                name: "contracts",
+                icon: "far fa-file-invoice-dollar",
+                permissions: ["contracts@control"]
+            },
+            {
+                text: i18n.t("admin.finbooks.title"),
+                name: "finbooks",
+                icon: "far fa-book",
+                permissions: ["finbooks@control"]
+            },
+            {
+                text: i18n.t("admin.trades.title"),
+                name: "trades",
+                icon: "far fa-analytics",
+                permissions: ["trades@view"]
+            },
+            {
+                text: i18n.t("admin.smartorders.title"),
+                name: "smartorders",
+                icon: "far fa-store",
+                permissions: ["smartorders@control"]
+            },
+            {
+                text: i18n.t("admin.comments.title"),
+                name: "comments",
+                icon: "far fa-comment",
+                permissions: ["comments@control"]
+            },
+            {
+                text: i18n.t("admin.settings.title"),
+                name: "settings",
+                icon: "far fa-cog",
+                permissions: ["setting@control"],
+                level: 3
+            }
+        ]
     },
     {
         text: "Làm mới",
