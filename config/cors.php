2207b02815cc97c6322082e8ea67eb74bdd11953
Tho Nguyen <ntt0102@gmail.com>
2023-04-27 16:37:58 +0700

    fix

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://quytrevang.nguyenvanxuanphu.com'],
+    'allowed_origins' => [request()->getHost()],
 
     'allowed_origins_patterns' => [],
 
