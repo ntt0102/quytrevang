b67eb277556e324dd61f9772ba36d286ade29187
Tho Nguyen <ntt0102@gmail.com>
2023-04-27 17:48:52 +0700

    fix

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -19,8 +19,8 @@ class CORS
         return $next($request)
             // ->header('Access-Control-Allow-Origin', 'https://smartpro.vps.com.vn')
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Allow-Credentials', 'true')
-            ->header('Access-Control-Allow-Headers', 'Content-Type');
+            // ->header('Access-Control-Allow-Methods', '*')
+            // ->header('Access-Control-Allow-Credentials', 'true')
+            // ->header('Access-Control-Allow-Headers', 'Content-Type');
     }
 }
