0efc52b744e1ef855d3e7f369dff4b9503e2b57d
Tho Nguyen <ntt0102@gmail.com>
2023-05-13 15:09:07 +0700

    buil webauthn

---
+++ database/migrations/2021_05_09_121958_create_contracts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContractsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('code')->unique();
+            $table->unsignedBigInteger('user_code');
+            $table->unsignedBigInteger('principal');
+            $table->double('interest_rate', 8, 5);
+            $table->unsignedBigInteger('advance')->nullable();
+            $table->date('paid_at');
+            $table->date('withdrawn_at')->nullable();
+            $table->json('paid_docs')->default('[]');
+            $table->json('withdrawn_docs')->default('[]');
+            $table->unsignedBigInteger('confirmed_by')->nullable();
+            $table->timestamps();
+            $table->foreign('user_code')->references('code')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contracts');
+    }
+}
---
+++ database/migrations/2021_05_09_121958_create_trades_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTradesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trades', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedSmallInteger('amount');
+            $table->float('scores');
+            $table->unsignedInteger('revenue');
+            $table->unsignedInteger('loss');
+            $table->unsignedInteger('fees');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trades');
+    }
+}
