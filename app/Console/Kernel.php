8f25b6cc938c9aa8e0cd6941f53c5c7fb2b8b841
Tho Nguyen <ntt0102@gmail.com>
2023-05-16 15:26:47 +0700

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,8 +11,6 @@ use App\Jobs\UpdateVn30f1mSymbolJob;
 use App\Jobs\ReportTradingJob;
 use App\Jobs\ExportTradingJob;
 
-// use App\Services\Admin\OrderChartService;
-
 class Kernel extends ConsoleKernel
 {
     /**
@@ -20,10 +18,7 @@ class Kernel extends ConsoleKernel
      *
      * @var array
      */
-    protected $commands = [
-        // 'App\Console\Commands\BackupDatabaseCommand',
-        // 'App\Console\Commands\CleanSubscriptionCommand'
-    ];
+    protected $commands = [];
 
     /**
      * Define the application's command schedule.
@@ -44,21 +39,6 @@ class Kernel extends ConsoleKernel
         $schedule->job(new UpdateVn30f1mSymbolJob)->fridays()->at('08:30');
         $schedule->job(new ReportTradingJob)->dailyAt('14:46');
         $schedule->job(new ExportTradingJob)->dailyAt('14:46');
-
-
-        // $schedule->command('database:backup')->daily();
-        // $schedule->command('subscription:clean')->yearly();
-        // //
-        // $oCS = new OrderChartService();
-        // $schedule->call(function () use ($oCS) {
-        //     set_global_value('openingMarketFlag', $oCS->checkOpeningMarket() ? '1' : '0');
-        //     set_global_value('vn30f1m', $oCS->getVn30f1mSymbol());
-        // })->dailyAt('06:30');
-        // //
-        // $schedule->call(function () use ($oCS) {
-        //     $oCS->reportTrading();
-        //     $oCS->exportToCsv();
-        // })->dailyAt('14:46');
     }
 
     /**
