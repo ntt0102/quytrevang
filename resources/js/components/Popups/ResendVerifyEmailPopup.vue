065e4a97336bcd7a0a5a27d581ca8924548eb8e9
Tho Nguyen <ntt0102@gmail.com>
2023-05-19 11:27:27 +0700

    fix last popup

--- resources/js/components/Popups/ResendVerifyEmailPopup.vue
+++ resources/js/components/Popups/ResendVerifyEmailPopup.vue
@@ -1,69 +1,58 @@
 <template>
-  <div>
-    <DxPopup
-      ref="popup"
-      :width="300"
-      :height="270"
-      :showCloseButton="true"
-      :title="$t('components.resendVerifyEmail.title')"
-      :show-title="true"
-      @hiding="$mf.popRouteHistoryState"
+    <CorePopup
+        ref="popupRef"
+        :width="300"
+        :height="270"
+        class="resend-email-popup"
+        :title="$t('components.resendVerifyEmail.title')"
+        @shown="onShown"
+        @hidden="onHidden"
     >
-      <template #content>
-        <form class="resend-email-form" @submit.prevent="onSubmit">
-          <div class="description">
-            {{ $t("components.resendVerifyEmail.description").format(email) }}
-          </div>
-          <DxForm>
-            <DxItem
-              item-type="button"
-              :button-options="{
-                width: '100%',
-                type: 'default',
-                text: $t('components.resendVerifyEmail.resend'),
-                useSubmitBehavior: true,
-              }"
-            />
-          </DxForm>
+        <form @submit.prevent="onSubmit">
+            <div class="description">
+                {{ $t("components.resendVerifyEmail.description", [email]) }}
+            </div>
+            <DxForm>
+                <DxItem
+                    item-type="button"
+                    :button-options="{
+                        width: '100%',
+                        type: 'default',
+                        text: $t('components.resendVerifyEmail.resend'),
+                        useSubmitBehavior: true,
+                    }"
+                />
+            </DxForm>
         </form>
-      </template>
-    </DxPopup>
-  </div>
+    </CorePopup>
 </template>
-<script>
-import { mapGetters, mapActions } from "vuex";
+<script setup>
 import DxForm, { DxItem } from "devextreme-vue/form";
+import CorePopup from "./CorePopup.vue";
+import { ref, computed } from "vue";
+import { useStore } from "vuex";
 
-export default {
-  components: {
-    DxForm,
-    DxItem,
-  },
-  data() {
-    return {};
-  },
-  computed: {
-    ...mapGetters("Auth", ["email"]),
-    popup() {
-      return this.$refs.popup.instance;
-    },
-  },
-  methods: {
-    ...mapActions("Auth", ["resendEmail"]),
-    show() {
-      this.popup.show();
-      this.$mf.pushPopupToHistoryState(() => this.popup.hide());
-    },
-    onSubmit() {
-      this.resendEmail().then(() => {
-        this.popup.hide();
-      });
-    },
-  },
-};
+const store = useStore();
+const popupRef = ref(null);
+const email = computed(() => store.state.auth.user.email);
+
+function show() {
+    popupRef.value.show();
+}
+function onSubmit() {
+    store.dispatch("auth/resendEmail").then(() => {
+        popupRef.value.hide();
+    });
+}
+function onShown() {}
+function onHidden() {}
+
+defineExpose({ show });
 </script>
 <style lang="scss">
-.description {
-  margin-bottom: 30px;
+.resend-email-popup {
+    .description {
+        margin-bottom: 30px;
+    }
 }
 </style>
