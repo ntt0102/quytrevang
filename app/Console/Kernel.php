686c87296df4dc865dbc0ada972d6b8a38ba7015
Tho Nguyen <ntt0102@gmail.com>
2023-05-23 17:38:21 +0700

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,17 +28,16 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-
-        $schedule->command('queue:work --stop-when-empty')
-            ->everyMinute()
-            ->withoutOverlapping();
-
         $schedule->job(new BackupDatabaseJob)->daily();
         $schedule->job(new CleanSubscriptionJob)->yearly();
         $schedule->job(new UpdateOpeningMarketJob)->dailyAt('06:30');
         $schedule->job(new UpdateVn30f1mSymbolJob)->fridays()->at('08:30');
         $schedule->job(new ReportTradingJob)->dailyAt('14:46');
         $schedule->job(new ExportTradingJob)->dailyAt('14:46');
+
+        $schedule->command('queue:work --stop-when-empty')
+            ->everyMinute()
+            ->withoutOverlapping();
     }
 
     /**
