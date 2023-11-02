358ccacd3ad13767dfcbd13dca50cbdfd56cb131
Tho Nguyen <ntt0102@gmail.com>
2023-11-02 14:41:58 +0700

    fix

--- app/Jobs/UpdateVn30f1mSymbolJob.php
+++ app/Jobs/UpdateVn30f1mSymbolJob.php
@@ -36,36 +36,10 @@ class UpdateVn30f1mSymbolJob implements ShouldQueue
      */
     public function handle()
     {
-        if ($this->weekOfMonth() == 3) {
-            $client = new \GuzzleHttp\Client();
-            $url = "https://spwapidatafeed.vps.com.vn/pslistdata";
-            $res = $client->get($url);
-            $symbol = json_decode($res->getBody())[0];
-            set_global_value('vn30f1m', $symbol);
-        }
-    }
-
-    private function weekOfMonth($date = null)
-    {
-        $date = !$date ? time() : strtotime($date);
-        //Get the first day of the month.
-        $firstOfMonth = strtotime(date("Y-m-01", $date));
-        //Apply above formula.
-        return self::weekOfYear($date) - self::weekOfYear($firstOfMonth) + 1;
-    }
-
-    private function weekOfYear($date)
-    {
-        $weekOfYear = intval(date("W", $date));
-        if (date('n', $date) == "1" && $weekOfYear > 51) {
-            // It's the last week of the previos year.
-            return 0;
-        } else if (date('n', $date) == "12" && $weekOfYear == 1) {
-            // It's the first week of the next year.
-            return 53;
-        } else {
-            // It's a "normal" week.
-            return $weekOfYear;
-        }
+        $client = new \GuzzleHttp\Client();
+        $url = "https://spwapidatafeed.vps.com.vn/pslistdata";
+        $res = $client->get($url);
+        $symbol = json_decode($res->getBody())[0];
+        set_global_value('vn30f1m', $symbol);
     }
 }
