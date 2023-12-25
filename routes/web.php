24c87ca45004ed42599777b730efb0faaaff8137
Tho Nguyen <ntt0102@gmail.com>
2023-12-25 20:01:58 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,18 @@ Route::any('{all}', function () {
 })->where('all', '^((?!test|cache|migrate).)*?');
 
 Route::get('cache', function () {
-    exec('php ../artisan config:cache', $output, $return);
-    if ($return != 0) return dd($output);
-    return "Cache done!";
+    // exec('php ../artisan config:cache', $output, $return);
+    // if ($return != 0) return dd($output);
+    // return "Cache done!";
 });
 
 Route::get('migrate', function () {
-    set_time_limit(300);
-    exec('php ../artisan migrate:refresh --seed', $output, $return);
-    if ($return != 0) return dd($output);
-    exec('php ../artisan passport:install --force', $output, $return);
-    if ($return != 0) return dd($output);
-    return "Migrate done!";
+    // set_time_limit(300);
+    // exec('php ../artisan migrate:refresh --seed', $output, $return);
+    // if ($return != 0) return dd($output);
+    // exec('php ../artisan passport:install --force', $output, $return);
+    // if ($return != 0) return dd($output);
+    // return "Migrate done!";
 });
 
 Route::get('test', function () {
