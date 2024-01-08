d7c906c11062fa6db353123dec610c38f12cb307
Tho Nguyen <ntt0102@gmail.com>
2024-01-08 13:04:44 +0700

    fix

---
+++ public/build/assets/Files-aa73eb82.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./FileManager-20480a85.js";import{d as a,g as s,j as c,o as n}from"./app-3237ec81.js";const o={class:"content-block dx-card responsive-paddings"},p={__name:"Files",setup(i){const e=a({clientPath:"files"});return(r,l)=>(n(),s("div",o,[c(t,{clientPath:e.clientPath},null,8,["clientPath"])]))}};export{p as default};
