560814923a7a039a18bc980bc64826d701c10a19
Tho Nguyen <ntt0102@gmail.com>
2024-02-10 21:31:36 +0700

    fix

--- resources/js/store/modules/Trading/Stock.js
+++ resources/js/store/modules/Trading/Stock.js
@@ -1,10 +1,8 @@
 function initialState() {
     return {
         symbols: { hose: [], cash: [], index: [], mix: [], watch: [] },
-        status: { connection: true, position: 0, pending: false },
         chartData: [],
         isChartLoading: false,
-        copyists: [],
     };
 }
 
@@ -34,7 +32,8 @@ const actions = {
             axios
                 .post("trading/stock/clone-symbols", {}, { noLoading: true })
                 .then((response) => {
-                    resolve(response.data);
+                    dispatch("getSymbols");
+                    resolve();
                 });
         });
     },
@@ -70,89 +69,6 @@ const actions = {
                 });
         });
     },
-    getStatus({ commit, dispatch, getters, state, rootGetters }) {
-        return new Promise((resolve, reject) => {
-            axios
-                .post("trading/order/get-status", {}, { noLoading: true })
-                .then((response) => {
-                    commit("setStatus", response.data);
-                    resolve();
-                });
-        });
-    },
-    getAccountInfo({ commit, dispatch, getters, state, rootGetters }) {
-        return new Promise((resolve, reject) => {
-            axios
-                .post("trading/order/get-account-info", {}, { noLoading: true })
-                .then((response) => {
-                    resolve(response.data);
-                });
-        });
-    },
-    executeOrder({ commit, dispatch, getters, state, rootGetters }, data) {
-        if (!state.status.connection)
-            return Promise.resolve({ isOk: false, message: "notConnect" });
-        return new Promise((resolve, reject) => {
-            commit("setChartLoading", true);
-            axios
-                .post("trading/order/execute-order", data, {
-                    noLoading: true,
-                    notify: true,
-                })
-                .then((response) => {
-                    commit("setChartLoading", false);
-                    dispatch("getStatus");
-                    resolve(response.data);
-                });
-        });
-    },
-    getCopyistStatus({ commit, dispatch, getters, state, rootGetters }) {
-        return new Promise((resolve, reject) => {
-            axios
-                .post(
-                    "trading/order/get-copyist-status",
-                    {},
-                    { noLoading: true }
-                )
-                .then((response) => {
-                    commit("setOrderStatuses", response.data);
-                    resolve();
-                });
-        });
-    },
-    closePosition({ commit, dispatch, getters, state, rootGetters }, id) {
-        return new Promise((resolve, reject) => {
-            axios
-                .post("trading/order/close-position", { id })
-                .then((response) => {
-                    dispatch("getCopyistStatus");
-                    resolve(response.data);
-                });
-        });
-    },
-    report({ commit, dispatch, getters, state, rootGetters }) {
-        return new Promise((resolve, reject) => {
-            axios.post("trading/order/report", null).then((response) => {
-                resolve();
-            });
-        });
-    },
-    export({ commit, dispatch, getters, state, rootGetters }) {
-        return new Promise((resolve, reject) => {
-            axios.post("trading/order/export", null).then((response) => {
-                resolve();
-            });
-        });
-    },
-    cashflow({ commit, dispatch, getters, state, rootGetters }, symbol) {
-        return new Promise((resolve, reject) => {
-            axios
-                .post("trading/order/cashflow", { symbol })
-                .then((response) => {
-                    resolve(response.data);
-                });
-        });
-    },
     resetState({ commit }) {
         commit("resetState");
     },
@@ -167,12 +83,6 @@ const mutations = {
     },
     setSymbols(state, data) {
         state.symbols = data;
-    },
-    setOrderStatuses(state, data) {
-        state.copyists = data;
-    },
-    setConfig(state, data) {
-        state.config = data;
     },
     resetState(state) {
         state = Object.assign(state, initialState());
