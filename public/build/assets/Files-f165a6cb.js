41422e74de31c88b278161ec33cabc08839e9764
Tho Nguyen <ntt0102@gmail.com>
2024-09-24 19:47:32 +0700

    fix pattern

---
+++ public/build/assets/Files-f165a6cb.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./FileManager-9e538b13.js";import{a,b as s,e as c,o as n}from"./app-81969494.js";const o={class:"content-block dx-card responsive-paddings"},d={__name:"Files",setup(i){const e=a({clientPath:"files"});return(r,l)=>(n(),s("div",o,[c(t,{clientPath:e.clientPath},null,8,["clientPath"])]))}};export{d as default};
