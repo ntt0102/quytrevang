fe65670a582788a2e3cfd080b55e8aa4964b3803
Tho Nguyen <ntt0102@gmail.com>
2024-12-28 09:31:09 +0700

    rename

--- resources/lang/vi/Notifications/FilteredShare.php
+++ resources/lang/vi/Notifications/FilteredShare.php
@@ -5,6 +5,6 @@ return [
     'title' => 'Lọc cổ phiếu hoàn tất.',
     'actionUrl' => 'dau-tu-co-phieu?list=filter',
     'actionTitle' => 'Xem biều đồ',
-    'success' => 'Lọc được :count cổ phiếu trong rổ :group.',
-    'failed' => 'Tên rổ chỉ số không đúng.',
+    'success' => 'Lọc được :count cổ phiếu trong nhóm:group.',
+    'failed' => 'Tên nhóm chỉ số không đúng.',
 ];
