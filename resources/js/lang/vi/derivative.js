4565c5afbfb848769fffcf46b999ba10f2641871
Tho Nguyen <ntt0102@gmail.com>
2025-05-15 14:17:36 +0700

    ps fix

--- resources/js/lang/vi/derivative.js
+++ resources/js/lang/vi/derivative.js
@@ -28,8 +28,8 @@ const continuePattern = [
             "P > 125-Pđỏ",
             "P < Pmax",
             "Ps < 50",
-            "Ptím > 50",
-            "Plam < 50",
+            "Ptím OK",
+            "Plam OK",
         ],
     },
     {
@@ -71,8 +71,8 @@ const reversalPattern = [
             "P > 70",
             "P < Pmax",
             "Ps < 50",
-            "Ptím OK",
-            "Plam OK",
+            "Ptím > 50",
+            "Plam < 50",
         ],
     },
     {
