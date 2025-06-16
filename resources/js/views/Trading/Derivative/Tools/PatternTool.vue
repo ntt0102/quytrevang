86e37aa082cd0bac767988e30c981a9f0a296517
Tho Nguyen <ntt0102@gmail.com>
2025-06-16 10:18:34 +0700

    ps fix

--- resources/js/views/Trading/Derivative/Tools/PatternTool.vue
+++ resources/js/views/Trading/Derivative/Tools/PatternTool.vue
@@ -882,7 +882,7 @@ function calcContinueLitePattern() {
                     // pink
                     dT4 >= TR3 / trThreshold,
                     DE >= phase3.pr,
-                    rCDE >= 0.68,
+                    rCDE >= 0.5,
                     isTimeNotEqual(dT3, dT4),
                 ],
                 [
