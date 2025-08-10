ecff572848493cadb68eab55ce7aeeb39dc76445
Nguyen Truong Tho <ntt0102@gmail.com>
2025-08-10 12:13:18 +0000

    save

--- database/migrations/2025_04_12_084216_create_derivative_orders_table.php
+++ database/migrations/2025_04_12_084216_create_derivative_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('derivative_orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedTinyInteger('status')->default(0);
+            $table->string('type');
             $table->tinyInteger('side');
             $table->float('entry_price', 6, 1);
             $table->float('tp_price', 6, 1);
